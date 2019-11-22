9c5a626789b0d948eda8cf9e98b487650c649847
Marcelo Eduardo Redoschi <mredoschi@uol.com.br>
2019-11-22 19:21:04 +0100

    Rubocop lint, and some manual tweaks to take care of most warnings.
     	modified:   app/controllers/professionalfamilies_controller.rb
     	modified:   app/controllers/professions_controller.rb
    
      The corresponding seed file content was meant to follow Brazilian official guidelines,
      known as "CBO - Classificação Brasileira de Ocupações".

--- app/controllers/professionalfamilies_controller.rb
+++ app/controllers/professionalfamilies_controller.rb
@@ -1,127 +1,112 @@
+# frozen_string_literal: true
+
+# Originally based on "CBO - Classificação Brasileira de Ocupações"
 class ProfessionalfamiliesController < ApplicationController
-  before_action :set_professionalfamily, only: [:show, :edit, :update, :destroy]
+  before_action :set_professionalfamily, only: %i[show edit update destroy]
 
-	before_filter :authenticate_user! # By default... Devise
+  before_filter :authenticate_user! # By default... Devise
 
-	load_and_authorize_resource  # CanCan(Can)
+  load_and_authorize_resource # CanCan(Can)
 
   # GET /professionalfamilies
   def index
     @professionalfamilies = Professionalfamily.all
 
-		@title=t('list')+' '+t('activerecord.models.professionalfamily').pluralize
+    @title = t('list') + ' ' + t('activerecord.models.professionalfamily').pluralize
 
-  	if is_admin_or_manager(current_user)
+    if is_admin_or_manager(current_user)
 
-			@title=t('navbar_menu_name').pluralize.upcase
-      @title+=' | '+t('activerecord.models.professionalfamily').capitalize.pluralize
+      @title = t('navbar_menu_name').pluralize.upcase
+      @title += ' | ' + t('activerecord.models.professionalfamily').capitalize.pluralize
+
+    elsif is_local_admin(current_user)
+
+      @title = t('navbar_menu_name').pluralize.upcase
+      @title += ' | ' + t('activerecord.models.professionalfamily').capitalize.pluralize
 
     else
 
-			if is_local_admin(current_user)
-
-				@title=t('navbar_menu_name').pluralize.upcase
-        @title+=' | '+t('activerecord.models.professionalfamily').capitalize.pluralize
-
-  		else
-
-  			@title=t('my.mp').upcase+' '+tm('professionalfamily').pluralize.upcase
-
-  		end
+      @title = t('my.mp').upcase + ' ' + tm('professionalfamily').pluralize.upcase
 
     end
 
-  	@q = Professionalfamily.accessible_by(current_ability).ransack(params[:q])
-		@professionalfamilies=@q.result.page(params[:page]).per(10)
-		@numprofessionalfamilies=Professionalfamily.count
-  
+    @q = Professionalfamily.accessible_by(current_ability).ransack(params[:q])
+    @professionalfamilies = @q.result.page(params[:page]).per(10)
+    @numprofessionalfamilies = Professionalfamily.count
   end
 
   # GET /professionalfamilies/1
   def show
-  	@title=t('activerecord.models.professionalfamily').capitalize
+    @title = t('activerecord.models.professionalfamily').capitalize
 
- 		@professions=@professionalfamily.profession.page(params[:page]).per(10)
+    @professions = @professionalfamily.profession.page(params[:page]).per(10)
 
-#  	if !@professionalfamily.name.nil?
+    #    if !@professionalfamily.name.nil?
 
-#    	@title=@title+": "+@professionalfamily.name
+    #      @title=@title+": "+@professionalfamily.name
 
-#     end
+    #     end
 
-  	if is_admin_or_manager(current_user)
+    if is_admin_or_manager(current_user)
 
-			@title=t('navbar_menu_name').pluralize.upcase+" | "+t('activerecord.models.professionalfamily').capitalize+": "+@professionalfamily.name
+      @title = t('navbar_menu_name').pluralize.upcase + ' | ' + t('activerecord.models.
+        professionalfamily').capitalize + ': ' + @professionalfamily.name
+
+    elsif is_local_admin(current_user)
+
+      @title = t('navbar_menu_name').mb_chars.upcase + ' | ' + t('activerecord.models.
+        professionalfamily').capitalize
 
     else
 
- 	 		if is_local_admin(current_user)
+      @title = tm('professionalfamily').pluralize.upcase
 
-				@title=t('navbar_menu_name').mb_chars.upcase+' | '+t('activerecord.models.professionalfamily').capitalize
+      @title += ' : ' + @professionalfamily.name unless @professionalfamily.name.nil?
 
-			else
-
-				@title=tm('professionalfamily').pluralize.upcase
-
-				if !@professionalfamily.name.nil?
-
-					@title+=" : "+@professionalfamily.name
-
-				end
-
-			end
-
-		end
-
+    end
   end
 
   # GET /professionalfamilies/new
   def new
     @professionalfamily = Professionalfamily.new
 
-    if is_admin_or_manager(current_user)
+    @title = if is_admin_or_manager(current_user)
 
-			@title=t('navbar_menu_name').pluralize.upcase+" | "+t('actions.new.m')+" "+t('activerecord.models.professionalfamily')
+               t('navbar_menu_name').pluralize.upcase + ' | ' + t('actions.
+        new.m') + ' ' + t('activerecord.models.professionalfamily')
 
-		else
+             elsif is_local_admin(current_user)
 
-			if is_local_admin(current_user)
+               current_user.institution.name + ': ' + t('actions.new.m') + ' ' + t('activerecord.
+        models.professionalfamily')
 
-		    @title=current_user.institution.name+": "+t('actions.new.m')+" "+t('activerecord.models.professionalfamily')
+             else
 
-			else
+               current_user.institution.name + ': ' + t('actions.new.m') + ' ' + t('activerecord.
+        models.professionalfamily')
 
-		    @title=current_user.institution.name+": "+t('actions.new.m')+" "+t('activerecord.models.professionalfamily')
-
-			end
-
-		end
-
+             end
   end
 
   # GET /professionalfamilies/1/edit
   def edit
+    #     @title=t('noun.edit')+" "+t('activerecord.models.professionalfamily')
 
-#     @title=t('noun.edit')+" "+t('activerecord.models.professionalfamily')
+    @title = if is_admin_or_manager(current_user)
 
-    if is_admin_or_manager(current_user)
+               t('navbar_menu_name').pluralize.upcase + ' | ' + t('noun.
+        edit') + ' ' + t('activerecord.models.professionalfamily')
 
-				@title=t('navbar_menu_name').pluralize.upcase+' | '+t('noun.edit')+' '+t('activerecord.models.professionalfamily')
+             elsif is_local_admin(current_user)
 
-    	else
+               t('navbar_menu_name').pluralize.upcase + ' | ' + t('noun.
+        edit') + ' ' + t('activerecord.models.professionalfamily')
 
-			if is_local_admin(current_user)
+             else
 
-				@title=t('navbar_menu_name').pluralize.upcase+' | '+t('noun.edit')+' '+t('activerecord.models.professionalfamily')
+               t('noun.edit') + ' ' + tm('professionalfamily').pluralize
 
-			else
-
- 			  @title=t('noun.edit')+' '+tm('professionalfamily').pluralize
-
-			end
-
-  	end
-
+             end
   end
 
   # POST /professionalfamilies
@@ -129,7 +114,8 @@ class ProfessionalfamiliesController < ApplicationController
     @professionalfamily = Professionalfamily.new(professionalfamily_params)
 
     if @professionalfamily.save
-      redirect_to @professionalfamily, notice: t('activerecord.models.professionalfamily').capitalize+' '+t('created.m')+' '+t('succesfully')
+      redirect_to @professionalfamily, notice: t('activerecord.models.professionalfamily')
+        .capitalize + ' ' + t('created.m') + ' ' + t('succesfully')
     else
       render :new
     end
@@ -138,8 +124,9 @@ class ProfessionalfamiliesController < ApplicationController
   # PATCH/PUT /professionalfamilies/1
   def update
     if @professionalfamily.update(professionalfamily_params)
-     # redirect_to @professionalfamily, notice: 'Professionalfamily was successfully updated.'
-       redirect_to @professionalfamily, notice: t('activerecord.models.professionalfamily').capitalize+' '+t('updated.m')+' '+t('succesfully')
+      # redirect_to @professionalfamily, notice: 'Professionalfamily was successfully updated.'
+      redirect_to @professionalfamily, notice: t('activerecord.models.
+        professionalfamily').capitalize + ' ' + t('updated.m') + ' ' + t('succesfully')
     else
       render :edit
     end
@@ -148,18 +135,19 @@ class ProfessionalfamiliesController < ApplicationController
   # DELETE /professionalfamilies/1
   def destroy
     @professionalfamily.destroy
-    redirect_to professionalfamilies_url, notice: t('activerecord.models.professionalfamily').capitalize+' '+t('deleted.m')+' '+t('succesfully')
-
+    redirect_to professionalfamilies_url, notice: t('activerecord.models.
+      professionalfamily').capitalize + ' ' + t('deleted.m') + ' ' + t('succesfully')
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_professionalfamily
-      @professionalfamily = Professionalfamily.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def professionalfamily_params
-      params.require(:professionalfamily).permit(:name, :subgroup_id, :familycode, :pap, :medres)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_professionalfamily
+    @professionalfamily = Professionalfamily.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def professionalfamily_params
+    params.require(:professionalfamily).permit(:name, :subgroup_id, :familycode, :pap, :medres)
+  end
 end
--- app/controllers/professions_controller.rb
+++ app/controllers/professions_controller.rb
@@ -1,39 +1,41 @@
+# frozen_string_literal: true
+
+# Originally based on "CBO - Classificação Brasileira de Ocupações"
 class ProfessionsController < ApplicationController
-  before_action :set_profession, only: [:show, :edit, :update, :destroy]
+  before_action :set_profession, only: %i[show edit update destroy]
 
   # GET /professions
   # GET /professions.json
   def index
-   #  @professions = Profession.all
+    #  @professions = Profession.all
 
-    @title=t('definition').pluralize.mb_chars.upcase+" | "+t('activerecord.models.profession').pluralize.capitalize
+    @title = t('definition').pluralize.mb_chars.upcase + ' | ' + t('activerecord.models.
+      profession').pluralize.capitalize
 
-  	@search = Profession.accessible_by(current_ability).ransack(params[:q])
-  	@professions=@search.result.page(params[:page]).per(10)
-		@numprofessions=Profession.count
-    @numprofessionalfamilies=Professionalfamily.count
-  
+    @search = Profession.accessible_by(current_ability).ransack(params[:q])
+    @professions = @search.result.page(params[:page]).per(10)
+    @numprofessions = Profession.count
+    @numprofessionalfamilies = Professionalfamily.count
   end
 
   # GET /professions/1
   # GET /professions/1.json
   def show
-    	@title=t('definition').pluralize.mb_chars.upcase+" | "+t('activerecord.models.profession').capitalize+' | '+t('actions.show')
-
+    @title = t('definition').pluralize.mb_chars.upcase + ' | ' + t('activerecord.models.
+      profession').capitalize + ' | ' + t('actions.show')
   end
 
   # GET /professions/new
   def new
     @profession = Profession.new
-    @title=t('definition').pluralize.mb_chars.upcase+" | "+t('activerecord.models.profession').pluralize.capitalize+' | '+t('actions.new.f')
-
+    @title = t('definition').pluralize.mb_chars.upcase + ' | ' + t('activerecord.models.
+      profession').pluralize.capitalize + ' | ' + t('actions.new.f')
   end
 
   # GET /professions/1/edit
   def edit
-
-    	@title=t('definition').pluralize.mb_chars.upcase+" | "+t('activerecord.models.profession').capitalize+' | '+t('actions.edit')
-
+    @title = t('definition').pluralize.mb_chars.upcase + ' | ' + t('activerecord.models.
+      profession').capitalize + ' | ' + t('actions.edit')
   end
 
   # POST /professions
@@ -43,8 +45,11 @@ class ProfessionsController < ApplicationController
 
     respond_to do |format|
       if @profession.save
-#         format.html { redirect_to @profession, notice: 'Profession was successfully created.' }
-				 format.html { redirect_to @profession, notice: t('activerecord.models.profession').capitalize+' '+t('created.f')+' '+t('succesfully') }
+        # format.html { redirect_to @profession, notice: 'Profession was successfully created.' }
+        format.html do
+          redirect_to @profession, notice: t('activerecord.models.
+          profession').capitalize + ' ' + t('created.f') + ' ' + t('succesfully')
+        end
 
         format.json { render action: 'show', status: :created, location: @profession }
       else
@@ -59,7 +64,10 @@ class ProfessionsController < ApplicationController
   def update
     respond_to do |format|
       if @profession.update(profession_params)
-        format.html { redirect_to @profession, notice: t('activerecord.models.profession').capitalize+' '+t('updated.f')+' '+t('succesfully') }
+        format.html do
+          redirect_to @profession, notice: t('activerecord.models.
+          profession').capitalize + ' ' + t('updated.f') + ' ' + t('succesfully')
+        end
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -79,13 +87,15 @@ class ProfessionsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_profession
-      @profession = Profession.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def profession_params
-      params.require(:profession).permit(:name, :occupationcode, :pap, :medres, :professionalfamily_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_profession
+    @profession = Profession.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def profession_params
+    params.require(:profession).permit(:name, :occupationcode, :pap, :medres,
+                                       :professionalfamily_id)
+  end
 end
