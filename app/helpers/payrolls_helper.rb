4842a268037c272e7dda27318e55e119b12a065e
Marcelo Eduardo Redoschi <mredoschi@uol.com.br>
2019-12-29 16:57:21 +0100

    Continued to review payrolls helper, fine tuned
      	modified:   app/helpers/payrolls_helper.rb
      	modified:   spec/helpers/payrolls_helper_spec.rb

--- app/helpers/payrolls_helper.rb
+++ app/helpers/payrolls_helper.rb
@@ -1,79 +1,30 @@
+# frozen_string_literal: true
+
+# Revised December 2019
 module PayrollsHelper
+  # https://apidock.com/rails/v4.2.1/ActionView/Helpers/OutputSafetyHelper/safe_join
+  def warn_if_special(payroll)
+    txt = ''
 
+    txt = I18n.t('activerecord.attributes.payroll.special') if payroll.special?
 
-	def warn_if_special(payroll)
+    safe_join([txt])
+  end
 
-		txt=''
+  def display_payment_amount(payroll)
+    scholarship_amount = Scholarship.in_effect_for(payroll).first.amount
 
-		if payroll.special?
+    txt = if payroll.special?
 
-				txt+=ta('payroll.special')
-		end
+            I18n.t('activerecord.attributes.payroll.special') + ': ' \
+                 +humanized_money_with_symbol(scholarship_amount)
 
-		return txt.html_safe
+          else
 
-	end
+            humanized_money(scholarship_amount)
 
+          end
 
-	def compute_total_absences(registration)
-
-        @annotations.absences_for(registration)
-
-	end
-
-
-	def display_payment_amount(payroll)
-
-
-			scholarship_amount=Scholarship.in_effect_for(payroll).first.amount
-
-      prefix=''
-
- 			if payroll.special?
-
-				prefix=I18n.t('activerecord.attributes.payroll.special')+': '
-
-  		end
-
-				prefix+humanized_money(scholarship_amount).html_safe
-
-	end
-
-	def display_amount_if_regular(payroll)
-
-			txt=''
-
-			if payroll.regular?
-
-				txt=humanized_money(payroll.scholarship.amount)
-
-			else
-
-				txt='---'
-
-			end
-
-			return txt.html_safe
-
-	end
-
-	def display_amount_if_special(payroll)
-
-			txt=''
-
-			if payroll.special?
-
-				txt=humanized_money_with_symbol(payroll.amount)
-
-			else
-
-				txt='---'
-
-			end
-
-			return txt.html_safe
-
-	end
-
-
+    safe_join([txt])
+  end
 end
--- spec/helpers/payrolls_helper_spec.rb
+++ spec/helpers/payrolls_helper_spec.rb
@@ -3,99 +3,38 @@
 require 'rails_helper'
 
 describe PayrollsHelper, type: :helper do
-  let!(:payroll) { FactoryBot.create(:payroll, :personal_taxation, :special) }
+  include MoneyRails::ActionViewExtension
+
+  let!(:payroll) { FactoryBot.create(:payroll, :personal_taxation) }
   let!(:taxation) { FactoryBot.create(:taxation, :personal) }
   let!(:scholarship) { FactoryBot.create(:scholarship, :pap, finish: '2050-1-1') }
 
   let(:dt) { Time.zone.today - 60 }
 
-  it 'warn_if_special(payroll)' do
+  it '-warn_if_special(payroll)' do
     txt = ''
 
-    txt += ta('payroll.special') if payroll.special?
-    safe_txt = safe_join(txt)
+    txt = I18n.t('activerecord.attributes.payroll.special') if payroll.special?
+    safe_txt = safe_join([txt])
 
     expect(safe_txt).to eq(helper.warn_if_special(payroll))
   end
 
-  it 'display_payment_amount(payroll)' do
+  it '-display_payment_amount(payroll)' do
     scholarship_amount = Scholarship.in_effect_for(payroll).first.amount
 
-    # scholarships=Scholarship.where("? >= start and ? <= finish ",p.monthworked, p.monthworked)
+    txt = if payroll.special?
 
-    # puts scholarships.first
+            I18n.t('activerecord.attributes.payroll.special') + ': ' \
+              +humanized_money_with_symbol(scholarship_amount)
 
-    prefix = ''
+          else
 
-    prefix = I18n.t('activerecord.attributes.payroll.special') + ': ' if payroll.special?
+            humanized_money(scholarship_amount)
 
-    txt = prefix + humanized_money(scholarship_amount)
+          end
 
-    safe_txt = safe_join(txt)
-
+    safe_txt = safe_join([txt])
     expect(safe_txt).to eq(helper.display_payment_amount(payroll))
   end
-
-  #
-  #   def compute_total_absences(registration)
-  #
-  #         @annotations.absences_for(registration)
-  #
-  #   end
-  #
-  #   def display_payment_amount(payroll)
-  #
-  #       scholarship_amount=Scholarship.in_effect_for(payroll).first.amount
-  #
-  #       if payroll.special?
-  #
-  #         txt=I18n.t('activerecord.attributes.payroll.special')+': ' \
-  #         +humanized_money_with_symbol(payroll.amount)
-  #
-  #       else
-  #
-  #         txt=humanized_money(scholarship_amount)
-  #
-  #       end
-  #
-  #       return txt.html_safe
-  #
-  #   end
-  #
-  #   def display_amount_if_regular(payroll)
-  #
-  #       txt=''
-  #
-  #       if payroll.regular?
-  #
-  #         txt=humanized_money(payroll.scholarship.amount)
-  #
-  #       else
-  #
-  #         txt='---'
-  #
-  #       end
-  #
-  #       return txt.html_safe
-  #
-  #   end
-  #
-  #   def display_amount_if_special(payroll)
-  #
-  #       txt=''
-  #
-  #       if payroll.special?
-  #
-  #         txt=humanized_money_with_symbol(payroll.amount)
-  #
-  #       else
-  #
-  #         txt='---'
-  #
-  #       end
-  #
-  #       return txt.html_safe
-  #
-  #   end
-  #
 end
