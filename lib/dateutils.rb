c69330a960e37313c07932dd62dd4d65aeb41875
Marcelo Eduardo Redoschi <mredoschi@uol.com.br>
2020-01-30 21:30:22 +0100

    Adjusted: previous_weekday, regular_date_to_days_elapsed methods.
        Enhanced tests.
    
      	modified:   lib/dateutils.rb
      	modified:   spec/lib/dateutils_spec.rb

--- lib/dateutils.rb
+++ lib/dateutils.rb
@@ -1,70 +1,69 @@
+# frozen_string_literal: true
+
 # Provides for convenient date manipulation
 # refer to lib/dateutils_spec.rb
 module Dateutils
+
   def self.elapsed_to_regular_date(num_days_elapsed)
     Settings.dayone + num_days_elapsed.days
   end
 
-  def self.regular_to_elapsed(dt)
-    regular_date_to_days_elapsed(dt)
+  def self.regular_to_elapsed(specified_dt)
+    regular_date_to_days_elapsed(specified_dt)
   end
 
-  def self.days_elapsed_since(dt)
-    regular_date_to_days_elapsed(dt)
+  def self.days_elapsed_since(specified_dt)
+    regular_date_to_days_elapsed(specified_dt)
   end
 
-  def self.regular_date_to_days_elapsed(dt)
-    first_calendar_day = Settings.dayone
-
-    @days_elapsed = (dt - first_calendar_day).to_i if dt >= first_calendar_day
-
-    @days_elapsed
+  def self.regular_date_to_days_elapsed(specified_dt)
+    (specified_dt - Settings.dayone).to_i
   end
 
   def self.past_month_start
-    Date.today.beginning_of_month - 1.month
+    Time.zone.today.beginning_of_month - 1.month
   end
 
   def self.past_month_finish
-    (Date.today - 1.month).to_date.end_of_month
+    (Time.zone.today - 1.month).to_date.end_of_month
   end
 
   # d = Date
-  def self.weekday?(dt)
-    week_day = dt.wday # Weekday
+  def self.weekday?(specified_dt)
+    week_day = specified_dt.wday # Weekday
 
-    (week_day > 0 && week_day < 6)
+    (week_day.positive? && week_day < 6)
   end
 
   # Opposite method
-  def self.weekend?(d)
-    !weekday?(d)
+  def self.weekend?(specified_dt)
+    !weekday?(specified_dt)
   end
 
-  def self.day_of_the_week_num(dt)
-    dt.wday
+  def self.day_of_the_week_num(specified_dt)
+    specified_dt.wday
   end
 
-  # dt = Date
-  def self.previous_weekday(dt)
-    case Dateutils.day_of_the_week_num(dt)
-    when 0
-      dt - 2.day
-    when 6
-      dt - 1.day
-    else
-      dt
+  # specified_dt = Date
+  def self.previous_weekday(specified_dt)
+    case Dateutils.day_of_the_week_num(specified_dt)
+    when 1 # Monday
+      specified_dt - 3.days
+    when 0 # Sunday
+      specified_dt - 2.days
+    else # Tuesday (2) thru Saturday (6)
+      specified_dt - 1.day
     end
   end
 
-  def self.to_civil(dt)
-    Date.civil(dt.year, dt.month, dt.day)
+  def self.to_civil(specified_dt)
+    Date.civil(specified_dt.year, specified_dt.month, specified_dt.day)
   end
 
-  def self.holiday?(dt)
-    dt_civil = to_civil(dt)
+  def self.holiday?(specified_dt)
+    specified_dt_civil = to_civil(specified_dt)
 
-    Holidays.on(dt_civil, :br).present?
+    Holidays.on(specified_dt_civil, :br).present?
   end
 
   # Alias, for convenience
